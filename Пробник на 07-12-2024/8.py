3d4e367b5fabb16e0e9a6b6ee221da33150cbe12
Belyash86 <92972932+Belyash86@users.noreply.github.com>
2025-02-20 14:28:12 +0500

    9, 17

--- Пробник на 07-12-2024/8.py
+++ Пробник на 07-12-2024/8.py
@@ -1,6 +1,6 @@
 from itertools import permutations
 k = 0
-for p in permutations('АРТЁМ'):
+for p in set(permutations('АРТЁМ')):
     s = ''.join(p)
     if not(s[0] in 'АЁ' and s[-1] in 'АЁ'):
         k += 1
